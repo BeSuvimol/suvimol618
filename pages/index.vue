13cef97dd0290904871c3d7b04dac17191cc1c50
suvimol <bepuengpak@gmail.com>
2018-06-18 11:34:15 +0700

    update-index

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
   <div><h1><center>Profile</center></h1>
   <div><center><img src="pic/be.jpg" hight="200" width="200"></center>
   <div><h1><center>นางสาวสุวิมล  พึ่งพัก</center></h1>
-  <div><h1><center>ชื่อเล่น บี</center></h1>
-  <div><h1><center>อายุ 20 ปี</center></h1></div>
-  <div><h1><center>รหัสนักศึกษา 6142040618</center></h1>
+  <div><center>ชื่อเล่น บี</center>
+  <div><center>อายุ 20 ปี</center></div>
+  <div><center>เกิดวันที่ 26 สิงหาคม 2540</center>></div>
+  <div><center>ที่อยู่ 46 หมู่ 3 ตำบลหนองตาด อำเภอเมือง จังวัดบุรีรัมย์</center>
   </div>
   </div>
   </div>
