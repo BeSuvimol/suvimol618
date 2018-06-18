69649e072ce67d058ca53f7e7ce4e2ad415c23c7
suvimol <bepuengpak@gmail.com>
2018-06-18 11:20:17 +0700

    program

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+<div class="">
+  <div><h1><center>Profile</center></h1>
+  <div><center><img src="pic/be.jpg" hight="200" width="200"></center>
+  <div><h1><center>นางสาวสุวิมล  พึ่งพัก</center></h1>
+  <div><h1><center>ชื่อเล่น บี</center></h1>
+  <div><h1><center>อายุ 20 ปี</center></h1></div>
+  <div><h1><center>รหัสนักศึกษา 6142040618</center></h1>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </template>
+  <script>
+  export default{
+     data(){
+       return{
+         id : 1
+       }
+     }
+  }
